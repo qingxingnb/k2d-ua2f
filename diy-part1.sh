d2613069be91cfe9faf1a26f784494b84b2b0a7a
Yuan <46626577+965163757@users.noreply.github.com>
2021-09-26 16:12:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,8 +16,9 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
-
 git clone https://github.com/DFlinmo/UA2F package/UA2F
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/MentoHUST-OpenWrt-ipk
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
 cd package/UA2F
 git checkout 20210531T234622
 cd ../..
